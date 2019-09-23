ff9fb85c7c4f6a9413bc022f180daa6ea09f879a
Tani314 <tanitabassum299@gmail.com>
2019-09-23 12:14:19 -0400

    added pwd command

--- bash.js
+++ bash.js
@@ -2,6 +2,6 @@ process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
 
-  process.stdout.write('You typed: ' + cmd);
+  process.stdout.write(process.cwd());
   process.stdout.write('\nprompt > ');
 });
